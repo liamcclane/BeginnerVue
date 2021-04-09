9b1f2256f1c59af21a8762b92476d8ed849bd896
liamcclane <lia_mcclane@msn.com>
2021-04-09 09:46:52 -0700

    Final Changes
    restyled and retitled the generate button
    
    randomized user on each click
    
    appened the aboutme to continually duplicate on each click

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
             name: "Jane Doe",
             age: 26,
             profilePic: "https://randomuser.me/api/portraits/women/10.jpg",
-            aboutMe: "hello I am 26 years old and about to start a new job working in vue with php and larvel",
+            aboutMe: "hello I am about to start a new job with php and larvel",
             github: "https://github.com/liamcclane",
             gender: "female"
         }
@@ -15,21 +15,11 @@ const app = Vue.createApp({
         async getUser() {
             const res = await fetch('https://randomuser.me/api')
             const { results } = await res.json();
-            console.log(results);
-            if(this.name == "Jane Doe") {
-                this.name = 'Jason';
-                this.age = 55;
-                this.profilePic = "https://randomuser.me/api/portraits/men/10.jpg";
-                this.aboutMe = "SOMETHIGANDA Eadasdl lorem";
-                this.gender = "male"
-            } else {
-                this.name =  "Jane Doe";
-                this.age =  26;
-                this.profilePic =  "https://randomuser.me/api/portraits/women/10.jpg";
-                this.aboutMe =  "hello I am 26 years old and about to start a new job working in vue with php and larvel";
-                this.github =  "https://github.com/liamcclane";
-                this.gender =  "female";
-            }
+            // console.log(results);
+            this.aboutMe += this.aboutMe;
+            this.name = results[0].name.first + " " + results[0].name.last;
+            this.profilePic = results[0].picture.large
+            this.gender = results[0].gender;
         }
     }
 })
